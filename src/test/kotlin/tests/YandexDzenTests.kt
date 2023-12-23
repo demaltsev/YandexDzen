c755be60410ba6915f3559f80c4f94a865463db6
demaltsev <demaltcev@yandex.ru>
2023-12-23 12:44:31 +0300

    Commmit

--- src/test/kotlin/tests/YandexDzenTests.kt
+++ src/test/kotlin/tests/YandexDzenTests.kt
@@ -1,36 +1,68 @@
 package tests
 
 import base.BaseTest
-import com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat
+import com.microsoft.playwright.Page
+import io.qameta.allure.Allure
+import io.qameta.allure.Description
+import io.qameta.allure.Owner
+import org.junit.jupiter.api.DisplayName
 import org.testng.Assert
 import org.testng.annotations.Test
-import java.util.regex.Pattern
+import java.io.ByteArrayInputStream
+import java.nio.file.Paths
 
 
 class YandexDzenTests : BaseTest() {
 
-
+    @Owner("Denis Maltsev")
+    @DisplayName("Форма")
+    @Description("Заполнение формы")
     @Test
     fun firstTest() {
         page?.navigate("http://85.192.34.140:8081/")
-        page?.getByText("Elements")?.click();
-        page?.querySelector("//li[@id='item-0']/span[1]")?.click();
-        page?.fill("[id=userName]", "ThreadQA Test");
-        page?.fill("[id=userEmail]", "threadqa@gmail.com");
-        page?.fill("[id=currentAddress]", "somewhere");
-        page?.click("[id=submit]");
-
+        page?.getByText("Elements")?.click()
+        page?.querySelector("//li[@id='item-0']/span[1]")?.click()
+        page?.fill("[id=userName]", "ThreadQA Test")
+        page?.fill("[id=userEmail]", "threadqa@gmail.com")
+        page?.fill("[id=currentAddress]", "somewhere")
+        page?.click("[id=submit]")
         //Проверяем, что после заполнения формы, появился другой блок
-        Assert.assertTrue(page?.isVisible("[id=output]")!!);
+        Assert.assertTrue(page?.isVisible("[id=output]")!!)
         //Проверяем, что в появившемся блоке, текст содержит предыдущий текст
-        Assert.assertTrue(page?.locator("[id=name]")?.textContent()!!.contains("NOT ThreadQA Test"));
+        Assert.assertTrue(page?.locator("[id=name]")?.textContent()!!.contains("NOT ThreadQA Test"))
     }
 
-
+    @Owner("Denis Maltsev")
+    @DisplayName("Регистрация")
+    @Description("Проверка формы регистрации")
     @Test
-    fun secondTest() {
-        page?.navigate("https://dzen.ru/")
-        assertThat(page).hasTitle(Pattern.compile("Дзен"))
+    fun regTest() {
+        page?.navigate("http://85.192.34.140:8081/")
+        page?.getByText("Forms")?.click()
+        page?.getByText("Practice Form")?.click()
+        page?.fill("[id=firstName]", "Denis")
+        page?.fill("[id=lastName]", "Maltsev")
+        page?.fill("[id=userEmail]", "test@gmgg.ru")
+        page?.locator("//div[@class='custom-control custom-radio custom-control-inline'][1]")?.click()
+        page?.fill("[id=userNumber]", "4444444444")
+        page?.fill("[id=dateOfBirthInput]", "08 Dec 1998")
+        page?.locator("//div[@class='custom-control custom-checkbox custom-control-inline'][1]")?.click()
+        page?.locator("id=uploadPicture")?.setInputFiles(Paths.get("C:/Users/Deni/Desktop/Screenshot_2.png"))
+        page?.fill("[id=currentAddress]", "Moscow, Green Street, 5")
+        page?.getByText("Select State")?.click()
+        page?.getByText("NCR")?.click()
+        page?.getByText("Select City")?.click()
+        page?.getByText("Gurgaon")?.click()
+        Allure.addAttachment("Скриншот заполненной формы", ByteArrayInputStream(makeScreenshot()))
+        page?.click("[id=submit]")
+        Assert.assertEquals(page?.locator("id=example-modal-sizes-title-lg")?.textContent(), "Thanks for submitting the form")
+
+
     }
+
+
+
 
 }
+
+
